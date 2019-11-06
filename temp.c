2115b528a61cac36c80218146c01c098ebc98b76
daljeet1900 <57410409+daljeet1900@users.noreply.github.com>
2019-11-06 22:47:12 +0530

    Create temp.c

---
+++ temp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ 
+  float a,b;
+  printf("\n enter temp value in fahrenheit");
+  scanf("%f", &a);
+  b=(a-32)*5/9;
+  printf("b=%.3f\n", b);
+  return 0;
+} 
+  
+  
+  
+  
