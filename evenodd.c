d248608362e770739c2df4266ddea6540db06243
daljeet1900 <57410409+daljeet1900@users.noreply.github.com>
2019-11-06 22:48:15 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+  int a,b;
+  printf("enter value of a=");
+  scanf("%d", &a);
+  b=a%2;
+  
+  if (b==0)
+  printf("%d is even", a);
+  else  
+  printf("%d is odd", a);
+  return 0;
+}
+                                      
